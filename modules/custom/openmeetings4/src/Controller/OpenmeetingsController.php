4be948d83717431f325b087ae584408129e60ef6
vishal.sirsodiya <vishal.sirsodiya23@gmail.com>
2018-04-05 19:44:47 +0530

    Add demo module for axelerant

--- modules/custom/openmeetings4/src/Controller/OpenmeetingsController.php
+++ modules/custom/openmeetings4/src/Controller/OpenmeetingsController.php
@@ -1,21 +1,17 @@
 <?php
-/**
- * { item_description }
- */
 
-/**
-*
-*/
 namespace \Drupal\Openmeetings4\Controller;
 
-//use Drupal\Core\Controller\ControllerBase;
-
 /**
-*
-*/
-class OpenmeetingsController
-{
-  public function display_text(){
+ * Open meeting Controller.
+ */
+class OpenmeetingsController {
+
+  /**
+   * Open meeting coments.
+   */
+  public function display_text() {
     return t("Test Page");
   }
+
 }
